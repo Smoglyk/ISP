8bae24e86eee3de82d9dbfa1294fa00183c19776
Smoglyk <76232699+Smoglyk@users.noreply.github.com>
2021-03-25 05:39:21 +0300

    Add files via upload

---
+++ Новая папка/Lab2_string.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab2_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count1 = 0, count2 = 0;
+            string str = Console.ReadLine(), strReverse = " ", strStorage;
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                count1++;
+                if (str[i] == ' ' || i == 0)
+                {
+                    strStorage = str;
+                    strStorage = strStorage.Substring(i);
+                    
+                    if(i == 0)
+                        strReverse = strReverse.Insert(count2 + 1 , strStorage);
+                    else
+                        strReverse = strReverse.Insert(count2, strStorage);
+                    str = str.Remove(i);
+                    count2 = count1;
+                    count1 = 0;
+                }
+                
+            }
+            strReverse = strReverse.Remove(0, 1);
+
+            Console.WriteLine(strReverse);
+        } 
+    }
+}
