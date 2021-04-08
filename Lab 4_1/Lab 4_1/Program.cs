b01a5baeffbedf1c19951d39a327b5d511de20b8
Smoglyk <76232699+Smoglyk@users.noreply.github.com>
2021-04-08 03:23:19 +0300

    Add files via upload

---
+++ Lab 4_1/Lab 4_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MediaPlay
+{
+    class Program
+    {
+        [DllImport("winmm.dll")]
+        public static extern bool PlaySound(string sound, IntPtr hmod, int flags = 0x0001);
+
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Choose a sound: (1). If you want stop or puase music, push 'S' or 'P'. Exit 'E'");
+                string sound = Console.ReadLine();
+                while (!(sound == "1" || sound == "2" || sound == "3" || sound == "S" || sound == "E" || sound == "P"))
+                    sound = Console.ReadLine();
+                if(sound == "1")
+                {
+                    Console.WriteLine("Play a music");
+                    PlaySound("C:/Users/Jarvis/Downloads/default.wav", IntPtr.Zero);
+                }
+                if (sound == "E")
+                {
+                    break;
+                }     
+            }    
+            
+        }
+    }
+}
