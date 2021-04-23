527a36921e111fe127ff934b55add9974e34f828
Smoglyk <76232699+Smoglyk@users.noreply.github.com>
2021-04-23 16:23:06 +0300

    Update Person.cs

--- Lab6/Lab6/Person.cs
+++ Lab6/Lab6/Person.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 
-    public class Person : ListHealthy, IComparable<Person>, Plan
+    public class Person : ListHealthy, IComparable<Person>, IPlan
     {
         protected string Gender { get; set; }
         protected int Id { get; set; }
